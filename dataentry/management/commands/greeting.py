3a7615f4992031a0d2fe0f0df3d07e2b4494914d
vinodsagar260981 <vinodsagar0981@gmail.com>
2024-05-14 10:45:30 +0530

    created two custom commands hello world and greeting

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandParser
+
+class Command(BaseCommand):
+    help = "greets the user"
+
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str, help="Specifies user name")
+
+    def handle(self, *args, **kwargs):
+        name = kwargs['name']
+        greeting = f'Hello {name}, Good Morning'
+        self.stdout.write(self.style.SUCCESS(greeting))
